6df453a2ab81e477ca234f1e960c784331df0e81
Joao Guilherme <joaoguilhermefc10@gmail.com>
2025-07-10 01:19:01 -0300

    improvements

--- scripts.js
+++ scripts.js
@@ -6,6 +6,9 @@ const maxRange = document.querySelector("#maxRange")
 const resultChildren = document.querySelector(".numbers-result")
 const result = document.querySelector(".result")
 
+const repeatNumber = document.querySelector("#repeatNumber")
+const imgCheckbox = document.querySelector("#checkbox-default")
+
 
 form.onsubmit = (event) => {
     event.preventDefault()
@@ -28,26 +31,45 @@ form.onsubmit = (event) => {
 }
 
 
+// Modifica a imagem pela ação do usuário
+repeatNumber.addEventListener("change", () => {
+    if(repeatNumber.checked) {
+        imgCheckbox.setAttribute("src", "assets/icons/drag-default.svg" )
+    } else {
+        imgCheckbox.setAttribute("src", "assets/icons/drag-active.svg")
+    }
+})
 
+// Gera um número aleatório
 function getRandomNumber (newSortedNumber){
+
     return Math.floor(Math.random() * (newSortedNumber.maxRange - newSortedNumber.minRange + 1 )) + newSortedNumber.minRange
 }
 
+// Enquanto for menor que a quantidade de numeros que devem ser sorteados, gera números aleatórios
 function totalRandomNumbers(newSortedNumber) {
     let total = []
 
-    for (let i = 0; i < newSortedNumber.numToDraw; i++) {
+    for (let i = 0; i < newSortedNumber.numToDraw;) {
         const totalNumbers = getRandomNumber(newSortedNumber)
 
-        if (!total.includes(totalNumbers)) {
+        if (repeatNumber.checked) {
             total.push(totalNumbers)
+            i++
+        } else {
+            if (!total.includes(totalNumbers)) {
+                total.push(totalNumbers)
+                i++
+            }
         }
     }
+
 
     return total
 }
 
 
+// Criação dos elementos para exibir os números sorteados
 function createElements(numbersToDisplay) {
     resultChildren.innerHTML = ""
     numbersToDisplay.forEach(element => {
